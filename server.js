1704bbc088da8fba6c72cb65ca7f8309bd676b71
Raghav Rathi ( With RVR ) <ytwithrvr@gmail.com>
2022-01-12 00:47:23 +0530

    foo bar

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ app.get("/", (req, res) => {
 	res.send("Hello World!");
 });
 
+app.get("/foo", (req, res) => {
+	res.send("this is foo bar page");
+});
+
 app.listen(PORT, () => {
 	console.log(`Server is Running at ${PORT}`);
 });
